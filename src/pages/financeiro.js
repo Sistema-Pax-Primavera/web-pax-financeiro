e40a4f06ad6f6530a1305df4069a53481a838343
Mateuskrp <kronbauer62@gmail.com>
2024-05-27 10:08:19 -0300

    atualizacao no mecanismo de busca na tabela de movimentacoes e baixas

--- src/pages/financeiro.js
+++ src/pages/financeiro.js
@@ -1,14 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import './financeiro.css';
+import React from "react";
+import "./financeiro.css";
+import HeaderFinanceiro from "../components/header-financeiro";
+import ContainerGeral from "../components/container-geral";
+import SubcontainerGeral from "../components/subcontainer-geral";
+import ImageComponent from "../components/imagem-component";
+import Imagem01 from '../../assets/financeiro-imagem.png';
+import Title from "../components/title";
 
 const Financeiro = () => {
-
-    return (
-        <div className='container-financeiro'>
-            <p>Financeiro</p>
-        </div>
-
-    )
-}
+  return (
+    <ContainerGeral 
+      conteudo={
+        <>
+          <HeaderFinanceiro />
+          <SubcontainerGeral
+            width={'100%'}
+            height={'450px'}
+            alignItens={'center'}
+            justifyContent={'center'}
+             conteudo={
+              <>
+                <ImageComponent
+                  src={Imagem01}
+                  width={'50%'}
+                  display={'flex'}
+                  alignItens={'center'}
+                  justifyContent={'center'}
+                />
+                <Title conteudo={'Selecione uma opção do menu!'} fontSize={'20px'} color={'#006b33'} fontWeight={'700'}/>
+              </>
+            }
+          />
+        </>
+      } 
+    />
+  );
+};
 
 export default Financeiro;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Financeiro from "../pages/financeiro";
+import Movimentacoes from "../pages/movimentacoes";
+import Baixas from "../pages/baixas";
 
 const RoutesApp = () => (
     <BrowserRouter basename="pax-primavera/financeiro">
         <Routes>
             <Route exact path="*" element={<Financeiro />} />
+            <Route exact path="/movimentacoes" element={<Movimentacoes />} />
+            <Route exact path="/baixas" element={<Baixas />} />
         </Routes>
     </BrowserRouter>
 );
